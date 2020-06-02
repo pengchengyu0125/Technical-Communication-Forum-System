ab36247a49df1368a8828489fe42a23b7eabd245
pengchengyu0125 <767026826@qq.com>
2020-06-02 15:37:42 +0800

    first commit

---
+++ src/main/java/com/luntan/controller/AuthorizeController.java
@@ -0,0 +1,49 @@
+package com.luntan.controller;
+
+import com.luntan.dto.AccessTokenDTO;
+import com.luntan.dto.GithubUser;
+import com.luntan.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.Redirect.uri}")
+    private String redirectUri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        if(user != null){
+            //登陆成功，写cookie和session
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+
+        }else {
+            //登陆失败，重新登陆
+            return "redirect:/";
+        }
+    }
+}
---
+++ src/main/java/com/luntan/controller/GreetingController.java
@@ -0,0 +1,14 @@
+package com.luntan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
