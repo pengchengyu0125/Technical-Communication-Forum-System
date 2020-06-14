4740220d847818d08be4ed0d2dcbb076c237ca31
pengchengyu0125 <767026826@qq.com>
2020-06-14 12:59:50 +0800

    avatar

--- src/main/java/com/luntan/model/User.java
+++ src/main/java/com/luntan/model/User.java
@@ -1,5 +1,8 @@
 package com.luntan.model;
 
+import lombok.Data;
+
+@Data
 public class User {
     private Integer id;
     private String name;
@@ -7,52 +10,5 @@ public class User {
     private String token;
     private double gmtCreate;
     private double gmtModified;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAccountId() {
-        return accountId;
-    }
-
-    public void setAccountId(String accountId) {
-        this.accountId = accountId;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public double getGmtCreate() {
-        return gmtCreate;
-    }
-
-    public void setGmtCreate(double gmtCreate) {
-        this.gmtCreate = gmtCreate;
-    }
-
-    public double getGmtModified() {
-        return gmtModified;
-    }
-
-    public void setGmtModified(double gmtModified) {
-        this.gmtModified = gmtModified;
-    }
+    private String avatarUrl;
 }
