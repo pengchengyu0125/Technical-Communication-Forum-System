2c0ce5e703db350e6a24c4a9187e528804044ee4
pengchengyu0125 <767026826@qq.com>
2020-07-08 12:14:33 +0800

    secondReply

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -18,3 +18,23 @@ function post(){
         dataType: "json"
     });
 }
+
+function collapseComments(e) {
+    var id=e.getAttribute("data-id");
+    var comments=$("#comment-"+id);
+    //获取展开状态
+    var collapse=e.getAttribute("data-collapse");
+    if(collapse){
+        //折叠二级评论
+        comments.removeClass("in");
+        e.removeAttribute("data-collapse");
+        e.classList.remove("active");
+    }
+    else{
+        //展开二级评论
+        comments.addClass("in");
+        //标记二级评论状态
+        e.setAttribute("data-collapse","in");
+        e.classList.add("active");
+    }
+}
