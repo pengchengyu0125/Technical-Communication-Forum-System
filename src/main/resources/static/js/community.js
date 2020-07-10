a5a28a6676405f8fafb69df3236c6e47ebd1b92d
pengchengyu0125 <767026826@qq.com>
2020-07-10 11:48:55 +0800

    replyError

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -2,21 +2,31 @@ function post(){
     var postId=$("#post_id").val();
     var content=$("#comment_content").val();
     var commentator=$("#commentator").val();
+    comment2target(postId, 1, content, commentator);
+}
+
+function comment2target(targetId, type, content, commentator){
     $.ajax({
-        type: "POST",
-        url: "/comment",
-        contentType: 'application/json',
-        data: JSON.stringify({
-            "parentId": postId,
-            "content": content,
-            "type": 1,
-            "commentator": commentator
-        }),
-        success: function(){
-            window.location.reload();
-        },
-        dataType: "json"
-    });
+            type: "POST",
+            url: "/comment",
+            contentType: 'application/json',
+            data: JSON.stringify({
+                "parentId": targetId,
+                "content": content,
+                "type": type,
+                "commentator": commentator
+            }),
+            success: function(){
+                window.location.reload();
+            },
+            dataType: "json"
+        });
+}
+
+function comment(e) {
+    var commentId=e.getAttribute("data-id");
+    var content=$("#input-"+commentId).val();
+    comment2target(commentId, 2, content, commentator);
 }
 
 function collapseComments(e) {
